9c849c82b29d9a99be4169eba5e15c66f7ed3975
Spyke Lionel <social.lionel@gmail.com>
2025-01-29 16:37:14 +0100

    update string

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.json({
     email: process.env.EMAIL || "developer.ndilionel@gmail.com",
-    current_datetime: new Date().toISOString(),
+    current_datetime: new Date().toISOString().slice(0, 19) + "Z",
     github_url: "https://github.com/spykelionel/hng12-task1-profile.git",
   });
 });
