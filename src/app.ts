c22c4f5a5b12c1aabb4b8eb65daef49937a37b9a
Spyke Lionel <social.lionel@gmail.com>
2025-01-29 15:41:53 +0100

    add readme

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
+import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
-const app = express();
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({
+    email: process.env.EMAIL || "social.lionel@gmail.com",
+    current_datetime: new Date().toISOString(),
+    github_url: " https://github.com/spykelionel/hng12-task1-profile.git",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
